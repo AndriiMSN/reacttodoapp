f7e14ed78bbf39b29c99727883aa7b16b3641c70
Andrii M <andrewmosin8@gmail.com>
2020-07-05 18:41:05 +0300

    Updates

---
+++ precache-manifest.f7d1b8b5ef79323a1cbcda17d730fdfe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ca8f6dcddc7ba81218de4420ff284a7",
+    "url": "/reacttodoapp/index.html"
+  },
+  {
+    "revision": "f16dafbb8aefde433073",
+    "url": "/reacttodoapp/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "6329f5e5977fb304557d",
+    "url": "/reacttodoapp/static/css/main.938ce446.chunk.css"
+  },
+  {
+    "revision": "f16dafbb8aefde433073",
+    "url": "/reacttodoapp/static/js/2.29850a0f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/reacttodoapp/static/js/2.29850a0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6329f5e5977fb304557d",
+    "url": "/reacttodoapp/static/js/main.28346a4a.chunk.js"
+  },
+  {
+    "revision": "faaa81da989a1536a182",
+    "url": "/reacttodoapp/static/js/runtime-main.88341fcd.js"
+  }
+]);
